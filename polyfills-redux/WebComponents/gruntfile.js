b229724acae2926fdb32e04e17cb1008adbf949d
sjmiles <scott@milesmail.net>
2013-02-21 05:06:43 -0800

    improve grunt structure, add README.md about grunt

--- polyfills-redux/WebComponents/gruntfile.js
+++ polyfills-redux/WebComponents/gruntfile.js
@@ -29,8 +29,24 @@ module.exports = function(grunt) {
       }
     },
 		uglify: {
-			WebComponents: {
-			   options: {
+      CustomElements: {
+			  options: {
+					sourceMap: 'CustomElements/custom-elements-source-map.js'
+				},
+				files: {
+					'CustomElements/custom-elements.min.js': CustomElements
+				}
+      },			
+      ShadowDOM: {
+			  options: {
+					sourceMap: 'ShadowDOM/shadowdom-source-map.js'
+				},
+				files: {
+					'ShadowDOM/shadowdom.min.js': ShadowDOM
+				}
+      },			
+      WebComponents: {
+			  options: {
 					sourceMap: 'webcomponents-source-map.js'
 				},
 				files: {
@@ -45,6 +61,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   
   // tasks
-  grunt.registerTask('default', ['concat', 'uglify']);
+  grunt.registerTask('default', ['uglify']);
 };
 
