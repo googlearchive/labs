1179bcde29958c903b18cc4227f751940477a9cf
Daniel Freedman <dfreedm@google.com>
2013-03-13 10:49:45 -0700

    Clean up end of microtask API

--- endOfMicroTask/endOfMicroTask.js
+++ endOfMicroTask/endOfMicroTask.js
@@ -1,28 +1,31 @@
-(function(global) {
+(function() {
+  'use strict';
   var attr = 'trigger';
-  var e = document.createelement('div');
+  var e = document.createElement('div');
   var mo = window.MutationObserver || window.WebKitMutationObserver;
   var fns = [];
   var caller = function(f){ f(); };
   var o = new mo(function() {
-    fns.forEach(caller);
-    fns = [];
-    e.removeAttribute(attr);
-    o.takeRecords();
-  });
-  var emt = function(f) {
-    if (!fns.length) {
-      e.setAttribute(attr);
+    try {
+      fns.forEach(caller);
+    } finally {
+      fns = [];
+      e.removeAttribute(attr);
+      o.takeRecords();
     }
-    fns.push(f);
-  }
+  });
   o.observe(e, {attributes: true});
   Object.defineProperty(
-    Object.getPrototypeOf(global),
+    Object.getPrototypeOf(window),
     'endOfMicroTask',
     {
-      value: emt.bind(global),
+      value: function(f) {
+        if (!fns.length) {
+          e.setAttribute(attr);
+        }
+        fns.push(f);
+      },
       enumerable: true
     }
   );
-})(window);
+})();
