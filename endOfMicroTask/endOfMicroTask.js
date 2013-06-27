c9e12c4a0b948591616bfcfedc3c29bcf448c6a8
Daniel Freedman <dfreedm@google.com>
2013-06-27 15:07:36 -0700

    use textnode instead of element

--- endOfMicroTask/endOfMicroTask.js
+++ endOfMicroTask/endOfMicroTask.js
@@ -1,28 +1,26 @@
 (function() {
   'use strict';
-  var attr = 'trigger';
-  var e = document.createElement('div');
+  var n = document.createTextNode('');
   var mo = window.MutationObserver || window.WebKitMutationObserver;
+  var iteration = 0;
   var fns = [];
-  // avoid leaking forEach iteration arguments to f
-  var caller = function(f){ f(); };
+
   var o = new mo(function() {
-    // make sure we can clean up if a function throws
-    try {
-      fns.forEach(caller);
-    } finally {
-      fns = [];
-      // reset attribute for next round
-      e.removeAttribute(attr);
-      // prevent calling handler again because of attribute reset
-      o.takeRecords();
+    for (var i = 0; i < fns.length; i++) {
+      try {
+        // avoid leaking forEach iteration arguments to f
+        fns[i].call(window);
+      } catch(_) {
+      }
     }
+    fns = [];
   });
-  o.observe(e, {attributes: true});
-  function mutEMOT(f) {
+  o.observe(n, {characterData: true});
+
+  function EOMT(f) {
     if (!fns.length) {
       // trigger observer
-      e.setAttribute(attr);
+      n.textContent = iteration++;
     }
     fns.push(f);
   }
@@ -30,8 +28,9 @@
     Object.getPrototypeOf(window),
     'endOfMicroTask',
     {
-      value: mutEMOT.bind(),
+      value: EOMT.bind(),
       enumerable: true
     }
   );
+  window.emotNode = n;
 })();
