0d8462f95d846384e74099a20edacf9e5fb309ba
Daniel Freedman <dfreedm@google.com>
2013-06-27 17:33:49 -0700

    simpler, allow for eomt inside of eomt

--- endOfMicroTask/endOfMicroTask.js
+++ endOfMicroTask/endOfMicroTask.js
@@ -1,36 +1,26 @@
-(function() {
+(function(scope) {
   'use strict';
   var n = document.createTextNode('');
   var mo = window.MutationObserver || window.WebKitMutationObserver;
   var iteration = 0;
   var fns = [];
 
-  var o = new mo(function() {
-    for (var i = 0; i < fns.length; i++) {
-      try {
-        // avoid leaking forEach iteration arguments to f
-        fns[i].call(window);
-      } catch(_) {
-      }
+  function callbackHandler() {
+    var l = fns.length, fn;
+    for (var i = 0; i < l; i++) {
+      fn = fns.shift();
+      fn.call(window);
     }
-    fns = [];
-  });
+  }
+
+  var o = new mo(callbackHandler);
   o.observe(n, {characterData: true});
 
   function EOMT(f) {
-    if (!fns.length) {
-      // trigger observer
-      n.textContent = iteration++;
-    }
+    n.textContent = iteration++;
     fns.push(f);
   }
-  Object.defineProperty(
-    Object.getPrototypeOf(window),
-    'endOfMicroTask',
-    {
-      value: EOMT.bind(),
-      enumerable: true
-    }
-  );
-  window.emotNode = n;
-})();
+
+  scope.endOfMicroTask = EOMT.bind();
+  scope.eomtNode = n;
+})(window);
