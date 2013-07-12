a65b17b4efcc17c771884e66bed45560138ddb83
Yvonne Yip <ykyyip@google.com>
2013-07-12 15:32:07 +0100

    scrub: revive basic scrub

--- scrubbing/src/Scrub.js
+++ scrubbing/src/Scrub.js
@@ -1,3 +1,10 @@
+/**
+ * TODO:
+ *   - add easing to snap
+ *   - test startDelay
+ *   - multiple iterations for input animation
+ *   - test CustomEffect
+ */
 (function(scope) {
   scope = scope || {};
   /* utils */
@@ -35,29 +42,17 @@
       this.snapPoints = [];
     }
     if (this.animation) {
-      this.animation.pause();
-      // TODO(sorvell): apply a minimum value to make sure initial keyframe is applied
-      this.currentTime = this.nudgeTime;
-      // then go back to 0 asap
-      webkitRequestAnimationFrame(function() {
-        this.currentTime = 0;
-      }.bind(this));
+      // create a player and pause the animation
+      document.timeline.play(this.animation).paused = true;
     } else {
       console.warn('No animation set for scrub');
     }
-    // setup animation segment.
-    this.easeOutTiming = new TimingFunction(this.easeOut);
-    this.easeInTiming = new TimingFunction(this.easeIn);
-    this.segment = new SeqGroup([], {fillMode: 'both', 
-      timingFunction: this.easeOutTiming});
   };
 
   scope.Scrub.prototype = {
     scale: 1,
     scrubType: 'x',
     wrap: false,
-    easeIn: [0.7, 0, 1, 1],
-    easeOut: [0, 0, 0.3, 1],
     nudgeTime: 1e-6,
     get node () {
       return this._node;
@@ -82,18 +77,18 @@
         if (this.scrubType == 'zoom') {
           this.scrubProp = 'zoom';
           moreEvents = [
-            {type: 'tkgesturestart', handler: this.trackStart.bind(this)},
-            {type: 'tkgesture', handler: this.track.bind(this)},
-            {type: 'tkgestureend', handler: this.trackEnd.bind(this)}
+            {type: 'trackstart', handler: this.trackStart.bind(this)},
+            {type: 'track', handler: this.track.bind(this)},
+            {type: 'trackend', handler: this.trackEnd.bind(this)}
           ];
         } else {
           this.directionProp = this.scrubType + 'Direction';
           this.scrubProp = 'd' + this.scrubType;
           // TODO(sorvell): include flick.
           moreEvents = [
-            {type: 'tktrackstart', handler: this.trackStart.bind(this)},
-            {type: 'tktrack', handler: this.track.bind(this)},
-            {type: 'tktrackend', handler: this.trackEnd.bind(this)}
+            {type: 'trackstart', handler: this.trackStart.bind(this)},
+            {type: 'track', handler: this.track.bind(this)},
+            {type: 'trackend', handler: this.trackEnd.bind(this)}
           ];
         }
         this.scrubEvents = this.scrubEvents.concat(moreEvents);
@@ -197,7 +192,7 @@
           this.scrubEvent = e;
           invoke(this, "onStartScrub", this);
           if (!this.scrubbingStopped) {
-            this.playToSnap(this.forward);
+            this.playTo(3);
           }
           this.resetScrub();
         }
@@ -243,45 +238,47 @@
       this.playToSnap(-this.scale);
     },
     playTo: function(inTime, inDuration) {
-      //console.log('playTo', inTime);
       this.scrubAnimation();
-      var reversing = (inTime < this.currentTime);
-      if (reversing != this.segment._reversing) {
-        this.segment.reverse();
-        this.segment.timing.timingFunction = reversing ? this.easeInTiming : 
-          this.easeOutTiming;
+      var rate = inDuration ? Math.abs(inTime - t) / inDuration : 1;
+      var duration = this.animation.specified.iterationDuration;
+      var t = this.currentTime;
+      var reversing = (inTime < t);
+      if (reversing) {
+        this.segment = new SeqGroup([
+          new SeqGroup([this.animation], {direction: 'reverse'})
+        ], {
+          iterationDuration: (duration - inTime),
+          playbackRate: rate,
+          startDelay: -(duration - t)
+        });
+      } else {
+        this.segment = new SeqGroup([this.animation], {
+          iterationDuration: inTime,
+          playbackRate: rate,
+          startDelay: -t
+        });
       }
-      this.segment.timing.duration = Math.abs(inTime - this.currentTime);
-      this.segment.timing.playbackRate = inDuration ? this.segment.timing.duration / 
-        inDuration : 1;
-      this.animation.timing.startDelay = reversing ? -inTime : -this.currentTime;
-      this.segment.currentTime = 0;
-      // TODO(sorvell): bug: must play here to ensure the global tick keeps going
-      this.animation.play();
-      this.segment.add(this.animation);
-      this.segment.play();
-      //console.log('segment duration', this.segment.timing.duration, 'animation delay', this.animation.timing.startDelay);
+      document.timeline.play(this.segment);
     },
     scrubAnimation: function() {
-      if (this.animation.parentGroup == this.segment) {
-        var c = -this.animation.timing.startDelay + this.currentTime;
-        // TODO(sorvell): bug: remove doesn't seem to work but can use add.
-        this.segment.parentGroup.add(this.animation);
-        this.animation.timing.startDelay = 0;
-        this.currentTime = c;
-        if (this.animation._reversing) {
-          this.animation.reverse();
-        }
-        this.animation.pause();
+      if (this.animation.parent) {
+        var reversing = this.animation.parent.specified.direction === 'reverse';
+        var segmentDuration = reversing ? this.segment.specified.iterationDuration % this.animation.specified.iterationDuration : this.segment.specified.iterationDuration;
+        // remove animation from segment
+        this.animation.remove();
+        this.segment.parent && this.segment.remove();
+        // reset the animation to the last segment duration
+        document.timeline.play(this.animation).paused = true;
+        this.currentTime = segmentDuration;
       }
     },
+    /*
     addAnimToSegment: function(inDirection, inSnaps) {
       var reversing = (inDirection*this.scale < 0);
       if (reversing != this.segment._reversing) {
         this.segment.reverse();
         // install proper easing
-        this.segment.timing.timingFunction = reversing ? this.easeInTiming : 
-            this.easeOutTiming;
+        this.segment.timing.timingFunction = reversing ? 'ease-in' : 'ease-out';
       }
       var start = inSnaps.start * this.duration, end = inSnaps.end * this.duration;
       this.segment.timing.duration = reversing ? this.currentTime - start :
@@ -291,9 +288,10 @@
       this.segment.currentTime = 0;
       // TODO(sorvell): bug: must play here to ensure the global tick keeps going
       //console.log('segment duration', this.segment.timing.duration, 'animation delay', this.animation.timing.startDelay);
-      this.animation.play();
+      document.timeline.play(this.animation);
       this.segment.add(this.animation);
     },
+    */
     set currentTime(inTime) {
       var t = inTime;
       if (this.wrap) {
@@ -304,17 +302,16 @@
       } else {
         t = clamp(t, 0, this.maxTime);
       }
-      this.animation.currentTime = t;
+      this.animation.player.currentTime = t;
     },
     get currentTime() {
       // TODO(sorvell): currentTime can drift(?) so make sure not to == duration 
       // or we'll pop over to next iteration.
-      return this.wrap ? this.animation.currentTime % this.duration : 
-        clamp(this.animation.currentTime, 0, this.maxTime);
+      return this.wrap ? this.animation.player.currentTime % this.duration : 
+        clamp(this.animation.player.currentTime, 0, this.maxTime);
     },
     get duration() {
-      return this.animation.animationDuration == Infinity ? this.animation.duration :
-        this.animation.animationDuration;
+      return this.animation.iterationDuration;
     },
     get maxTime() {
       return this.duration - (this.wrap ? 0 : this.nudgeTime);
