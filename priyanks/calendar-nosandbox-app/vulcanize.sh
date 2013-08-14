45ddb94f0c8dfa4fc720fe574833a1ece6c631b5
Daniel Freedman <dfreedm@google.com>
2013-08-14 14:12:42 -0700

    Fix sh script for filenames, use local polymer

--- priyanks/calendar-nosandbox-app/vulcanize.sh
+++ priyanks/calendar-nosandbox-app/vulcanize.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-node ../../vulcanize/vulcan.js -v --csp -i dev.html -o imports.html
+node ../../vulcanize/vulcan.js -v --csp -i index.html -o imports.html
 mv index-vulcanized.html app.html
