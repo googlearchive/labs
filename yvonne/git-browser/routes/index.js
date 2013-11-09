861584adfafe0c7d1df03779453d37b15fe25147
Yvonne Yip <ykyyip@google.com>
2013-11-08 17:25:34 -0800

    git-browser: show ahead/behind, custom root paths

--- yvonne/git-browser/routes/index.js
+++ yvonne/git-browser/routes/index.js
@@ -4,9 +4,14 @@
  */
 
  var fs = require('fs');
+ var path = require('path');
  var exec = require('exec-sync');
 
- var root = '/Users/ykyyip/Source/polymer-old';
+ var root = process.argv.splice(2)[0];
+ if (!root) {
+ 	throw 'Usage: node app.js <path-to-directory>';
+ }
+ root = path.resolve(root);
 
 exports.index = function(req, res){
 	fs.readdir(root, function(err, files) {
@@ -14,12 +19,16 @@ exports.index = function(req, res){
 		files.forEach(function(f) {
 			try {
 				var path = root + '/' + f;
-				var status = exec('git --git-dir=' + path + '/.git --work-tree=' + path + ' status');
+				var gitcmd = 'git --git-dir=' + path + '/.git --work-tree=' + path + ' ';
+				var status = exec(gitcmd + 'status');
+				var ab = exec(gitcmd + 'rev-list --left-right --count origin/master...HEAD');
 				repos.push({
 					name: f,
 					clean: status.indexOf('nothing to commit') !== -1,
 					untracked: status.indexOf('Untracked files') !== -1,
-					status: status
+					status: status,
+					behind: ab.split('	')[0],
+					ahead: ab.split('	')[1],
 				});
 			} catch (e) {}
 		});
